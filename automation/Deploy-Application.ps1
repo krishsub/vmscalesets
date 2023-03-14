cf398b09bbcb647dd7150b2111aea0cdb001982d
Krishnan Subramanian <krishsub@microsoft.com>
2023-03-14 08:27:07 +0100

    cleanup

--- automation/Deploy-Application.ps1
+++ automation/Deploy-Application.ps1
@@ -10,10 +10,8 @@ param (
 
 <#
     Uses blue/green deployment topology for update of a workload running on
-    Azure VMSS. Assumes one VMSS which is currently active ("active" prefix
-    below) and one VMSS which is currently inactive ("inactive" prefix below).
-    The active VMSS becomes inactive and the inactive VMSS becomes active
-    after the execution of this script.
+    Azure VMSS. Uses "current" and "vNext" to indicate what is current and
+    what will be the next version of the workload. 
 #>
 
 $extensionName = "AppInstallExtension"
@@ -22,63 +20,67 @@ $warningPreference = "SilentlyContinue"
 $storageAccount = Get-AzStorageAccount -ResourceGroupName $ResourceGroupName
 $autoScaleSettings = Get-AzAutoscaleSetting -ResourceGroupName $ResourceGroupName
 
-# Find the autoscale which has zero instances = inactive
-# Find the autoscale which has non-zero instances = active
+# Find the autoscale which has non-zero instances = current (version)
+# Find the autoscale which has zero instances =vNext (version)
 foreach ($item in $autoScaleSettings) {
     $someResource = Get-AzResource -ResourceId $item.TargetResourceUri
     $someVmss = Get-AzVmss -ResourceGroupName $someResource.ResourceGroupName -VMScaleSetName $someResource.ResourceName 
-    # sanity check; actual VMSS count should be zero <-- matches auto-scale 0 default
+    # sanity check; actual VMSS count should be zero for "vNext"
     if ($item.Profile[0].CapacityDefault -eq 0 -and $someVmss.Sku.Capacity -eq 0) {
-        $inactiveAutoScaleProfile = $item
+        $vNextAutoScaleProfile = $item
     }
-    # actual VMSS count should be non-zero <-- matches auto-scale non-zero default
+    # actual VMSS count should be non-zero for "current"
     if ($item.Profile[0].CapacityDefault -gt 0 -and $someVmss.Sku.Capacity -gt 0) {
-        $activeAutoScaleProfile = $item
+        $currentAutoScaleProfile = $item
     }
 }
 
 # Ensure state of resource group is consistent for performing blue/green deployment
 # Should be 2 profiles for CPU autoscale, a VMSS with non-zero instances, 
 # a VMSS with zero instance and a single storage account.
-if ($inactiveAutoScaleProfile -and $activeAutoScaleProfile -and $storageAccount -and $storageAccount.Count -eq 1) {
+if ($currentAutoScaleProfile -and $vNextAutoScaleProfile -and $storageAccount -and $storageAccount.Count -eq 1) {
     # Get and save autoscale settings for both VMSS
-    $activeCapacityMininum = $activeAutoScaleProfile.Profile[0].CapacityMinimum
-    $activeCapacityDefault = $activeAutoScaleProfile.Profile[0].CapacityDefault
-    $activeCapacityMaximum = $activeAutoScaleProfile.Profile[0].CapacityMaximum
+    $vNextCapacityMininum = $vNextAutoScaleProfile.Profile[0].CapacityMinimum
+    $vNextCapacityDefault = $vNextAutoScaleProfile.Profile[0].CapacityDefault
+    $vNextCapacityMaximum = $vNextAutoScaleProfile.Profile[0].CapacityMaximum
 
-    $inactiveCapacityMininum = $inactiveAutoScaleProfile.Profile[0].CapacityMinimum
-    $inactiveCapacityDefault = $inactiveAutoScaleProfile.Profile[0].CapacityDefault
-    $inactiveCapacityMaximum = $inactiveAutoScaleProfile.Profile[0].CapacityMaximum
+    $currentCapacityMininum = $currentAutoScaleProfile.Profile[0].CapacityMinimum
+    $currentCapacityDefault = $currentAutoScaleProfile.Profile[0].CapacityDefault
+    $currentCapacityMaximum = $currentAutoScaleProfile.Profile[0].CapacityMaximum
 
+    # Get the VMSS associated with "current" autoscale
+    $currentVmssResource = Get-AzResource -ResourceId $currentAutoScaleProfile.TargetResourceUri
+    $currentVmss = Get-AzVmss -ResourceGroupName $currentVmssResource.ResourceGroupName -VMScaleSetName $currentVmssResource.ResourceName
+    $currentCapacity = $currentVmss.Sku.Capacity
+    Write-Output "'Current': $($currentVmss.Name), capacity: $currentCapacity"
+
+    # Get the VMSS associated with "vNext" autoscale
+    $vNextVmssResource = Get-AzResource -ResourceId $vNextAutoScaleProfile.TargetResourceUri
+    $vNextVmss = Get-AzVmss -ResourceGroupName $vNextVmssResource.ResourceGroupName -VMScaleSetName $vNextVmssResource.ResourceName
+    Write-Output "'vNext': $($vNextVmss.Name)"
+
+    # Prevent auto-scale from doing anything till "vNext" is handling load, so remove it
     Write-Output "Removing autoscale settings during deployment"
-    # Prevent auto-scale from doing anything till the new version is handling load
-    Remove-AzAutoscaleSetting -ResourceGroupName $ResourceGroupName -Name $inactiveAutoScaleProfile.Name | Out-Null
-    Remove-AzAutoscaleSetting -ResourceGroupName $ResourceGroupName -Name $activeAutoScaleProfile.Name | Out-Null
+    Remove-AzAutoscaleSetting -ResourceGroupName $ResourceGroupName -Name $currentAutoScaleProfile.Name | Out-Null
+    Remove-AzAutoscaleSetting -ResourceGroupName $ResourceGroupName -Name $vNextAutoScaleProfile.Name | Out-Null
     Write-Output "Removed autoscale settings during deployment"
 
-    # Get the VMSS associated with inactive autoscale
-    $inactiveResource = Get-AzResource -ResourceId $inactiveAutoScaleProfile.TargetResourceUri
-    $inactiveVmss = Get-AzVmss -ResourceGroupName $inactiveResource.ResourceGroupName -VMScaleSetName $inactiveResource.ResourceName
-
-    # Get the VMSS associated with active autoscale
-    $activeResource = Get-AzResource -ResourceId $activeAutoScaleProfile.TargetResourceUri
-    $activeVmss = Get-AzVmss -ResourceGroupName $activeResource.ResourceGroupName -VMScaleSetName $activeResource.ResourceName      
-
-    # We don't update an extension, we remove it and add it (assuming release folder will change)
+    # Ensure "vNext" is clean without an extension
     try {
-        Write-Output "Removing extension from $($inactiveVmss.Name)"
-        Remove-AzVmssExtension -VirtualMachineScaleSet $inactiveVmss -Name $extensionName | Out-Null
-        $inactiveVmss | Update-AzVmss | Out-Null
-        Write-Output "Removed extension from $($inactiveVmss.Name)"
+        Write-Output "Removing extension: $extensionName from 'vNext': $($vNextVmss.Name)"
+        Remove-AzVmssExtension -VirtualMachineScaleSet $vNextVmss -Name $extensionName | Out-Null
+        Write-Output "Removed extension: $extensionName from 'vNext': $($vNextVmss.Name), updating VMSS"
+        $vNextVmss | Update-AzVmss | Out-Null
     }
     catch {
-        Write-Output "No extension found with name $extensionName, will continue"
+        Write-Output "No extension: $extensionName from 'vNext': $($vNextVmss.Name) to remove, will continue"
     }
+    
     # Files to downloaded to VMSS, note that when using folders, the file name should be qualified in "commandToExecute"
     $settings = @{
         "fileUris"         = (
             "$($storageAccount.PrimaryEndpoints.Blob)$BlobContainerName/install.ps1",
-            "$($storageAccount.PrimaryEndpoints.Blob)$BlobContainerName/xyzazcopy.exe",
+            "$($storageAccount.PrimaryEndpoints.Blob)$BlobContainerName/azcopy.exe",
             "https://download.visualstudio.microsoft.com/download/pr/ff197e9e-44ac-40af-8ba7-267d92e9e4fa/d24439192bc549b42f9fcb71ecb005c0/dotnet-hosting-7.0.3-win.exe"
         );
         "commandToExecute" = "powershell -ExecutionPolicy Unrestricted -File install.ps1 $($storageAccount.PrimaryEndpoints.Blob)$BlobContainerName  $ReleaseFolderName"
@@ -92,74 +94,122 @@ if ($inactiveAutoScaleProfile -and $activeAutoScaleProfile -and $storageAccount 
     };
 
     try {        
-        # Scale-out new version to old version's capacity 
-        Write-Output "Scaling-out $($inactiveVmss.Name) from 0 to $($activeVmss.Sku.Capacity)"
-        $inactiveVmss.Sku.Capacity = $activeVmss.Sku.Capacity
-        # below can throw an exception if the script extension failed
-        $inactiveVmss | Update-AzVmss | Out-Null  
-        Write-Output "Scale-out $($inactiveVmss.Name) completed"
+        # Scale-out "vNext" to "current" capacity 
+        Write-Output "Scaling-out 'vNext': $($vNextVmss.Name) from 0 to $currentCapacity"
+        $vNextVmss.Sku.Capacity = $currentCapacity
+        $vNextVmss | Update-AzVmss | Out-Null  
+        Write-Output "Scaling-out 'vNext': $($vNextVmss.Name) from 0 to $currentCapacity complete"
 
-        # Update inactive VMSS with the extension pointing to latest app version
-        Write-Output "Adding extension to VMSS $($inactiveVmss.Name)"
-        $inactiveVmss = Add-AzVmssExtension `
-            -VirtualMachineScaleSet $inactiveVmss `
+        # Update "vNext" VMSS with the extension pointing to latest app version
+        Write-Output "Adding extension to 'vNext': $($vNextVmss.Name)"
+        $vNextVmss = Add-AzVmssExtension `
+            -VirtualMachineScaleSet $vNextVmss `
             -Name $extensionName `
             -Publisher "Microsoft.Compute"  `
             -Type "CustomScriptExtension" `
             -TypeHandlerVersion "1.9" `
             -Setting $settings `
             -ProtectedSetting $protectedSettings `
-            -ProvisionAfterExtension @("IaaSAntimalware", "MicrosoftMonitoringAgent", "DependencyAgentWindows")
-        $inactiveVmss | Update-AzVmss
-        Write-Output "Added extension to VMSS $($inactiveVmss.Name)"
+            -ProvisionAfterExtension @("IaaSAntimalware", "AzureMonitorWindowsAgent", "DependencyAgentWindows")
+        $vNextVmss | Update-AzVmss | Out-Null
+        Write-Output "Added extension: $extensionName to 'vNext': $($vNextVmss.Name)"
         
-        # Scale in the active VMSS (old app version) to 0
-        Write-Output "Scaling-in $($activeVmss.Name) from $($activeVmss.Sku.Capacity) to 0"
-        $activeVmss.Sku.Capacity = 0
-        $activeVmss | Update-AzVmss | Out-Null
-        Write-Output "Scale-in $($activeVmss.Name) completed"
+        # Scale in "current" to 0
+        Write-Output "Scaling-in 'current': $($currentVmss.Name) from $($currentVmss.Sku.Capacity) to 0"
+        $currentVmss.Sku.Capacity = 0
+        $currentVmss | Update-AzVmss | Out-Null
+        Write-Output "Scale-in 'current': $($currentVmss.Name) completed to 0"
 
-        # Swap autoscale profiles between active & inactive VMSS
-        $inactiveAutoScaleProfile.Profile[0].CapacityMinimum = $activeCapacityMininum
-        $inactiveAutoScaleProfile.Profile[0].CapacityDefault = $activeCapacityDefault
-        $inactiveAutoScaleProfile.Profile[0].CapacityMaximum = $activeCapacityMaximum
+        # Swap autoscale profiles between "current" & "vNext" VMSS
+        $currentAutoScaleProfile.Profile[0].CapacityMinimum = $vNextCapacityMininum
+        $currentAutoScaleProfile.Profile[0].CapacityDefault = $vNextCapacityDefault
+        $currentAutoScaleProfile.Profile[0].CapacityMaximum = $vNextCapacityMaximum
 
-        $activeAutoScaleProfile.Profile[0].CapacityMinimum = $inactiveCapacityMininum
-        $activeAutoScaleProfile.Profile[0].CapacityDefault = $inactiveCapacityDefault
-        $activeAutoScaleProfile.Profile[0].CapacityMaximum = $inactiveCapacityMaximum
+        $vNextAutoScaleProfile.Profile[0].CapacityMinimum = $currentCapacityMininum
+        $vNextAutoScaleProfile.Profile[0].CapacityDefault = $currentCapacityDefault
+        $vNextAutoScaleProfile.Profile[0].CapacityMaximum = $currentCapacityMaximum
         
-        # Add the auto-scale settings back
-        $inactiveAutoScaleProfile | New-AzAutoscaleSetting | Out-Null
-        $activeAutoScaleProfile | New-AzAutoscaleSetting | Out-Null
+        # Add the auto-scale settings back which we removed earlier
+        Write-Output "Adding autoscale settings back"
+        $currentAutoScaleProfile | New-AzAutoscaleSetting | Out-Null
+        $vNextAutoScaleProfile | New-AzAutoscaleSetting | Out-Null
+        Write-Output "Added autoscale settings back"
         
-        Write-Output "################## Done scale swap ##################"
+        Write-Output "################## Done vNext update ##################"
     }
     catch {
-        # Updating inactive VMSS threw an error, attempt rollback to old state
-        Write-Error "################## Error - rolling back changes ##################"
-        $inactiveVMsExtensionState = ($inactiveVmss | Get-AzVmssVM).Resources.ProvisioningState
-        if ($inactiveVMsExtensionState -notcontains 'Succeeded') {
-            Write-Output "################## Extension failure, rollback ##################"
-            # Extension failed, remove it
-            Remove-AzVmssExtension -VirtualMachineScaleSet $inactiveVmss -Name $extensionName | Update-AzVmss | Out-Null
-            # set back the capacity to 0
-            $inactiveVmss.Sku.Capacity = 0
-            $inactiveVmss | Update-AzVmss | Out-Null
+        Write-Output "################## Exception ##################"
+        Write-Output $_.Exception
+        Write-Output "################## Exception ##################"
+        # "vNext" VMSS threw an error, attempt rollback to old state
+        # Wrap individual actions in try / catch to ensure we attempt to rollback as much as possible
+        Write-Output "################## Error, attempting rollback ##################"
+        
+        $vNextExtensionStates = ($vNextVmss | Get-AzVmssVM).Resources.ProvisioningState
+        # Check if one of the extensions failed
+        if ($vNextExtensionStates -contains 'Failed') {
+            Write-Output "Removing extension: $extensionName from 'vNext': $($vNextVmss.Name)"
+            try {
+                # Extension failed, remove it
+                Remove-AzVmssExtension -VirtualMachineScaleSet $vNextVmss -Name $extensionName | Update-AzVmss | Out-Null
+            }
+            catch {
+                Write-Output "Couldn't remove extension: $extensionName from 'vNext': $($vNextVmss.Name)"
+            }
 
-            $inactiveAutoScaleProfile.Profile[0].CapacityMinimum = $inactiveCapacityMininum
-            $inactiveAutoScaleProfile.Profile[0].CapacityDefault = $inactiveCapacityDefault
-            $inactiveAutoScaleProfile.Profile[0].CapacityMaximum = $inactiveCapacityMaximum
+            try {
+                # set back the capacity to the original value
+                $currentVmss.Sku.Capacity = $currentCapacity
+                Write-Output "Reverting 'current': $($currentVmss.Name) to capacity: $currentCapacity"
+                $currentVmss | Update-AzVmss | Out-Null
+                Write-Output "Reverted 'current': $($currentVmss.Name) to capacity: $currentCapacity"
+            }
+            catch {
+                Write-Output "Failed to revert 'current': $($currentVmss.Name) to capacity: $currentCapacity"
+            }
+            
+            try {
+                # set back the capacity to 0
+                $vNextVmss.Sku.Capacity = 0
+                Write-Output "Reverting 'vNext': $($vNextVmss.Name) to capacity: 0"
+                $vNextVmss | Update-AzVmss | Out-Null
+                Write-Output "Reverted 'vNext': $($vNextVmss.Name) to capacity: 0"
+            }
+            catch {
+                Write-Output "Failed to revert 'vNext': $($vNextVmss.Name) to capacity: 0"
+            }
 
-            $activeAutoScaleProfile.Profile[0].CapacityMinimum = $activeCapacityMininum
-            $activeAutoScaleProfile.Profile[0].CapacityDefault = $activeCapacityDefault
-            $activeAutoScaleProfile.Profile[0].CapacityMaximum = $activeCapacityMaximum
+            # set back the autoscale settings
+            $currentAutoScaleProfile.Profile[0].CapacityMinimum = $currentCapacityMininum
+            $currentAutoScaleProfile.Profile[0].CapacityDefault = $currentCapacityDefault
+            $currentAutoScaleProfile.Profile[0].CapacityMaximum = $currentCapacityMaximum
 
-            $inactiveAutoScaleProfile | New-AzAutoscaleSetting | Out-Null
-            $activeAutoScaleProfile | New-AzAutoscaleSetting | Out-Null
-            Write-Output "################## Extension failure, rollback complete ##################"
+            $vNextAutoScaleProfile.Profile[0].CapacityMinimum = $vNextCapacityMininum
+            $vNextAutoScaleProfile.Profile[0].CapacityDefault = $vNextCapacityDefault
+            $vNextAutoScaleProfile.Profile[0].CapacityMaximum = $vNextCapacityMaximum
+
+            try {
+                Write-Output "Adding back autoscale for 'current': $($currentVmss.Name)"
+                $currentAutoScaleProfile | New-AzAutoscaleSetting | Out-Null
+                Write-Output "Added back autoscale for 'current': $($currentVmss.Name)"
+            }
+            catch {
+                Write-Output "Failed to add back autoscale for 'current': $($currentVmss.Name)"
+            }
+
+            try {
+                Write-Output "Adding back autoscale for 'vNext': $($vNextVmss.Name)"
+                $vNextAutoScaleProfile | New-AzAutoscaleSetting | Out-Null
+                Write-Output "Added back autoscale for 'vNext': $($vNextVmss.Name)"
+            } 
+            catch {
+                Write-Output "Failed to add back autoscale for 'vNext': $($vNextVmss.Name)"
+            }
+            Write-Output "################## Rollback complete, check state ##################"
         }
         else {
-        
+            # None of the extensions seem to have failed? Yet, an error?
+            Write-Output "################## No rollback action possible, unknown error, check state ##################"
         }
     }
 }
--- automation/install.ps1
+++ automation/install.ps1
@@ -1,57 +1,93 @@
 param($storageBlob, $releaseFolderName)
 
+$ErrorActionPreference = "Stop"
+
 Start-Transcript
 
-# Windows features
-Install-WindowsFeature Web-Server -IncludeManagementTools
+try {
+    # Windows features
+    Write-Output "Installing Windows Features..."
+    Install-WindowsFeature Web-Server -IncludeManagementTools
+    Write-Output "Finished installing Windows Features..."
+}
+catch {
+    Stop-Transcript
+    throw
+}
 
-# Install .NET Core 5.x 
-.\dotnet-hosting-7.0.3-win.exe /install /quiet /norestart /log dotnetlog.txt
+try {
+    # Install .NET Core 5.x 
+    Write-Output "Installing .NET hosting Features..."
+    .\dotnet-hosting-7.0.3-win.exe /install /quiet /norestart /log dotnetlog.txt
+    Write-Output "Finished installing .NET hosting Features..."
+}
+catch {
+    Stop-Transcript
+    throw
+}
 
-# Delay 30 seconds for .NET Core install to finish
-Start-Sleep -Seconds 30
+try {
+    # Delay 30 seconds for .NET Core install to finish
+    Write-Output "Sleeping 30 seconds for .NET Core install to finish..."
+    Start-Sleep -Seconds 30
 
-# Restart IIS
-net stop was /y
-net start w3svc
+    # Restart IIS
+    Write-Output "Restart IIS"
+    net stop was /y
+    net start w3svc
 
-# Delay 10 seconds
-Start-Sleep -Seconds 10
+    # Delay 10 seconds
+    Write-Output "Sleeping 10 seconds for IIS to restart..."
+    Start-Sleep -Seconds 10
 
-# Reconfigure App Pool
-Start-IISCommitDelay
-$pool = Get-IISAppPool -Name DefaultAppPool
-$pool.ManagedRuntimeVersion = ""
-Stop-IISCommitDelay
+    # Reconfigure App Pool
+    Write-Output "Reconfiguring App Pool"
+    Start-IISCommitDelay
+    $pool = Get-IISAppPool -Name DefaultAppPool
+    $pool.ManagedRuntimeVersion = ""
+    Stop-IISCommitDelay
+    Write-Output "Reconfigured App Pool"
 
-# Delay 10 seconds
-Start-Sleep -Seconds 10
+    # Delay 10 seconds
+    Write-Output "Sleeping 10 seconds for App Pool to restart..."
+    Start-Sleep -Seconds 10
 
-Stop-WebAppPool DefaultAppPool
-net stop was /y
-net start w3svc
-Start-WebAppPool DefaultAppPool
+    Write-Output "Starting restart of IIS after AppPool reconfigure"
+    Stop-WebAppPool DefaultAppPool
+    net stop was /y
+    net start w3svc
+    Start-WebAppPool DefaultAppPool
+    Write-Output "Completed restart of IIS after AppPool reconfigure"
 
-# Delay 10 seconds
-Start-Sleep -Seconds 10
+    # Delay 10 seconds
+    Start-Sleep -Seconds 10
 
-# Deploy the app
-Stop-WebAppPool DefaultAppPool
-.\azcopy login --identity
-.\azcopy cp $storageBlob/$releaseFolderName/* C:\inetpub\wwwroot --recursive
-Start-WebAppPool DefaultAppPool
+    # Deploy the app
+    Write-Output "Deploying app"
+    Stop-WebAppPool DefaultAppPool
+    .\azcopy login --identity
+    .\azcopy cp $storageBlob/$releaseFolderName/* C:\inetpub\wwwroot --recursive
+    Start-WebAppPool DefaultAppPool
+    Write-Output "Deployed app"
 
-# Delay 10 seconds
-Start-Sleep -Seconds 10
+    # Delay 10 seconds
+    Start-Sleep -Seconds 10
 
-# App Pool recycle after .NET Core App Deploy
-# Restart-Computer -Force
-Stop-WebAppPool DefaultAppPool
-net stop was /y
-net start w3svc
-Start-WebAppPool DefaultAppPool
+    # App Pool recycle after .NET Core App Deploy
+    # Restart-Computer -Force
+    Write-Output "Restarting IIS after app deploy"
+    Stop-WebAppPool DefaultAppPool
+    net stop was /y
+    net start w3svc
+    Start-WebAppPool DefaultAppPool
+    Write-Output "Restarted IIS after app deploy"
 
-# Delay 10 seconds
-Start-Sleep -Seconds 10
+    # Delay 10 seconds
+    Start-Sleep -Seconds 10
 
-Stop-Transcript
+    Stop-Transcript
+}
+catch {
+    Stop-Transcript
+    throw
+}
